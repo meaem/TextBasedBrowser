9b67674c706d44e2500da4e19336cc5e532d6961
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-05-20 15:48:10 +0200

    stage 6 completed and clean

--- Text-Based Browser/task/browser.py
+++ Text-Based Browser/task/browser.py
@@ -1,4 +1,3 @@
-# import argparse
 import sys
 import os
 import requests
@@ -7,11 +6,9 @@ from colorama import Fore
 
 
 def is_valid_url(user_input):
-    # return user_input.lower().startswith('https://') and\
     return '.' in user_input
 
 
-# write your code here
 def convert_to_file_name(url):
     url = url.replace('https://', '')
     return url[0:url.rindex('.')]
@@ -40,55 +37,34 @@ def send_get_request(url):
         url = "https://" + url
 
     response = requests.get(url, headers={'User-Agent': user_agent})
-    # print(response.headers)
-    # response.encoding = 'utf-8'
-    # print("****response.encoding*****",response.encoding)
-
     return response.text
 
 
-# bloomberg.com
-# nytimes.com
-# back
 def get_human_readable(html):
     soup = BeautifulSoup(html)
-    # print(soup.prettify())
     all_tags = soup.find_all(["p", "h1", "h2", "h3", "h4", "h5", "h6", "a", "ul", "ol", "li"])
-    # for a in all_tags:
-    #     print(a.name)
     return "\n".join(
         [Fore.BLUE + a.get_text() + Fore.BLACK if a.name == 'a' else a.get_text() for a in all_tags if a.get_text()])
 
 
 def browse(url, cache_dir, history):
-    # print(f"browsing: {url}")
     if is_valid_url(url):
         history.append(url)
-        html = send_get_request(url)  # .rstrip("\n")
-        # idx = html.find(chr(160))
-        # with open('ttt.txt ','a') as ff:
-        # print("@@@@@@@@@@",idx,file=ff)
-        # for c in html[idx-1:idx+2]:
-        #     print(c , ord(c),file=ff)
-
-        # print(chr(160))
+        html = send_get_request(url)
         txt = get_human_readable(html)
         print(txt)
         save(txt, convert_to_file_name(url), cache_dir)
     else:
-        # print_cached_page(url, cache_dir, history)
         print("Incorrect URL")
 
 
 def main():
-    # print(sys.argv)
     history = []
 
     if len(sys.argv) != 2:
         print("Invalid call to the program, please specify a directory to save pages")
         return
     cache_dir = sys.argv[1]
-    # print(os.access(cache_dir, mode=os.F_OK))
     if not os.access(cache_dir, mode=os.F_OK):
         os.mkdir(cache_dir)
 
@@ -98,8 +74,6 @@ def main():
             break
 
         if url == "back":
-            # print(len(history))
-            # print(history)
             if len(history) > 0:
                 history.pop()
 
